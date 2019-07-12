4808543bbe849c94d28e08ecf2b9a0ba8b4949f6
Robrecht Cannoodt <rcannood@gmail.com>
2019-07-12 16:26:40 +0200

    roxygenise ssa.btl

--- R/ssa.btl.R
+++ R/ssa.btl.R
@@ -1,38 +1,66 @@
-# Copyright 2007, 2008, 2010 Mario Pineda-Krch.
-#
-# This file is part of the R package GillespieSSA.
-#
-# GillespieSSA is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# GillespieSSA is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with GillespieSSA.  If not, see <http://www.gnu.org/licenses/>.
-
-`ssa.btl` <-
-function(x = stop("missing state vector (x)"),
-         a = stop("missing propensity vector (a)"),
-        nu = stop("missing state-change matrix (nu)"),
-         f = stop("missing coarse-graining factor (f)")) { # See p.4 in Chatterjee et al. (2005)
-
+#' Binomial tau-leap method (BTL)
+#'
+#' Binomial tau-leap method implementation of the \acronym{SSA} as described
+#' by Chatterjee et al. (2005). It is usually called from within \code{\link{ssa}},
+#' but can be invoked directly.
+#'
+#' @param x state vector.
+#' @param a vector of evaluated propensity functions.
+#' @param nu state-change matrix.
+#' @param f coarse-graining factor (see page 4 in Chatterjee et al. 2005).
+#'
+#' @details Performs one time step using the Binomial tau-leap method. Intended to be invoked by \code{\link{ssa}}.
+#'
+#' @return A list with two elements:
+#' * the time leap (`tau`) and
+#' * the realized state change vector (`nu_j`).
+#'
+#' @references Chatterjee et al. (2005)
+#'
+#' @seealso [GillespieSSA-package], [ssa()]
+#'
+#' @examples
+#' a <- function(parms,x){
+#'   b <- parms[1]
+#'   d <- parms[2]
+#'   K <- parms[3]
+#'   N <- x[1]
+#'   return(c(b*N , N*b + (b-d)*N/K))
+#' }
+#' parms <- c(2,1,1000,500)
+#' x <- 500
+#' nu <- matrix(c(+1, -1),ncol=2)
+#' t <- 0
+#' for (i in seq(100)) {
+#'   out <- ssa.btl(x, a(parms, x), nu, f = 10)
+#'   x <- x + out$nu_j
+#'   t <- t + 1
+#'   cat("t:",t,", x:",x,"\n")
+#' }
+#'
+#' @importFrom stats rpois rbinom
+#'
+#' @keywords misc datagen ts
+#'
+#' @export
+ssa.btl <- function(
+  x = stop("missing state vector (x)"),
+  a = stop("missing propensity vector (a)"),
+  nu = stop("missing state-change matrix (nu)"),
+  f = stop("missing coarse-graining factor (f)")
+) {
   coercing <- FALSE
 
   # Calculate tau
-  tau <- f/sum(a)   # Calculate the current tau
+  tau <- f / sum(a)   # Calculate the current tau
   if (tau>1) tau<-1 # Tau cannot be larger than unity!
 
   M <- length(a)    # Number of reaction channels
-  tilde_x <- x    
+  tilde_x <- x
   nu_j <- matrix(rep(0,length(x)))
- 
-  # Loop over all reaction channels having propensity fun>0 
-  for (j in seq(M)[a>0]) {    
+
+  # Loop over all reaction channels having propensity fun>0
+  for (j in seq(M)[a>0]) {
     if (any(nu[,j]<0)) { # do this if there are limiting reactions
       mask <- nu[,j]<0
       L <- min(floor(tilde_x[mask]/abs(nu[mask,j])))
@@ -40,9 +68,9 @@ function(x = stop("missing state vector (x)"),
         p <- 1
         coercing <- TRUE
       }
-      else p <- a[j]*tau/L  
+      else p <- a[j]*tau/L
       k <- rbinom(1,L,p)
-    } 
+    }
     else { # do this if there are no limiting reactions
       k <- rpois(1,(a[j]*tau))
     }
@@ -52,13 +80,16 @@ function(x = stop("missing state vector (x)"),
     tilde_x <- tilde_x + tmp_nu_j
 
     # Record the current state change (it is returned by ssa.btl)
-    nu_j <- nu_j + tmp_nu_j    
+    nu_j <- nu_j + tmp_nu_j
   } # for()
 
-  # Throw a warning message if p was coerced to unity. Coercing implies too 
+  # Throw a warning message if p was coerced to unity. Coercing implies too
   # large steps-size due to a high coarse-graining factor (f)
-  if(coercing) warning("coerced p to unity - consider lowering f")
+  if (coercing) warning("coerced p to unity - consider lowering f")
 
-  return(list(tau=tau, nu_j=nu_j))
+  list(
+    tau = tau,
+    nu_j = nu_j
+  )
 }
 
