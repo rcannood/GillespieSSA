7ce600e970e3ebed1272c22cfbe6a4954db29431
Robrecht Cannoodt <rcannood@gmail.com>
2019-07-12 15:42:03 +0200

    fix ssa documentation

--- R/ssa.R
+++ R/ssa.R
@@ -26,71 +26,77 @@
 #'           maxWallTime = Inf
 #' )
 #'
-#' @details Although \code{ssa} can be invoked by only specifying the system
-#' arguments (initial state vector \code{x0}, propensity vector \code{a},
-#' state-change matrix \code{nu}), the final time (\code{tf}), and the
+#' @details Although `ssa` can be invoked by only specifying the system
+#' arguments (initial state vector `x0`, propensity vector `a`,
+#' state-change matrix `nu`), the final time (`tf`), and the
 #' \acronym{SSA} method to use, substantial improvements in speed and accuracy
-#' can be obtained by adjusting the additional (and optional) \code{ssa}
-#' arguments. By default \code{ssa} (tries to) use conservative default values
+#' can be obtained by adjusting the additional (and optional) `ssa`
+#' arguments. By default `ssa` (tries to) use conservative default values
 #' for the these arguments, prioritizing computational accuracy over
-#' computational speed. These default values are, however, \strong{not} fool
+#' computational speed. These default values are, however, **not** fool
 #' proof for the approximate methods, and occasionally one will have to hand
 #' tweak them in order for a stochastic model to run appropriately.
 #'
 #' @param x0 numerical vector of initial states where the component elements
 #'   must be named using the same notation as the corresponding state variable in
-#'   the propensity vector, \code{a}.
+#'   the propensity vector, `a`.
 #' @param a character vector of propensity functions where state variables
-#'   correspond to the names of the elements in \code{x0}.
+#'   correspond to the names of the elements in `x0`.
 #' @param nu numerical matrix of change if the number of individuals in each
 #'   state (rows) caused by a single reaction of any given type (columns).
 #' @param parms named vector of model parameters.
 #' @param tf final time.
 #' @param method text string indicating the \acronym{SSA} method to use,
 #' the valid options are:
-#' * \code{"D"} --- Direct method (default method),
-#' * \code{"ETL"} - Explicit tau-leap,
-#' * \code{"BTL"} --- Binomial tau-leap, or
-#' * \code{"OTL"} --- Optimized tau-leap.
+#' * `"D"` --- Direct method (default method),
+#' * `"ETL"` - Explicit tau-leap,
+#' * `"BTL"` --- Binomial tau-leap, or
+#' * `"OTL"` --- Optimized tau-leap.
 #' @param simName optional text string providing an arbitrary name/label for
 #' the simulation.
-#' @param tau step size for the \code{ETL} method (\eqn{>0}).
-#' @param f coarse-graining factor for the \code{BTL} method (\eqn{>1}) where a
+#' @param tau step size for the `ETL` method (\eqn{>0}).
+#' @param f coarse-graining factor for the `BTL` method (\eqn{>1}) where a
 #'   higher value results in larger step-size.
-#' @param epsilon accuracy control parameter for the \code{OTL} method (\eqn{>0}).
-#' @param nc critical firing threshold for the \code{OTL} method (positive integer).
+#' @param epsilon accuracy control parameter for the `OTL` method (\eqn{>0}).
+#' @param nc critical firing threshold for the `OTL` method (positive integer).
 #' @param hor numerical vector of the highest order reaction for each species where
-#'   \eqn{\mathtt{hor} \in \{1,2,22\}}{hor=(1,2,22)}. Setting \code{hor=NaN} uses
-#'   the default \code{hor=rep(22,N)} where \code{N} is the number of species (See
-#'   page 6 in Cao et al. 2006). Unless \code{hor=NaN} the number of elements must
-#'   equal the number of states \eqn{N}. Only applicable in the \code{OTL} method.
-#' @param dtf \code{D} method threshold factor for the \code{OTL} method. The
-#' \code{OTL} method is suspended if \code{tau} it estimates is smaller than the
-#'   \code{dtf} multiple of the \code{tau} that the \code{D} method would have used
+#'   \eqn{\mathtt{hor} \in \{1,2,22\}}{hor=(1,2,22)}. Setting `hor=NaN` uses
+#'   the default `hor=rep(22,N)` where `N` is the number of species (See
+#'   page 6 in Cao et al. 2006). Unless `hor=NaN` the number of elements must
+#'   equal the number of states \eqn{N}. Only applicable in the `OTL` method.
+#' @param dtf `D` method threshold factor for the `OTL` method. The
+#' `OTL` method is suspended if `tau` it estimates is smaller than the
+#'   `dtf` multiple of the `tau` that the `D` method would have used
 #'   (i.e. \eqn{\tau_{\mathtt{OTL}} < \mathtt{dtf} \times \tau_{\mathtt{D}}}{tau_OTL<dtf*\tau_D})
 #'   (See step 3, page 3 in Cao et al. 2006).
-#' @param nd number of single-reaction steps performed using the Direct method during \code{otl} suspension (See step 3, page 3, Cao et al. 2006).
-#'   \item{ignoreNegativeState}{boolean object indicating if negative state values should be ignored (this can occur in the \code{etl} method). If \code{ignoreNegativeState=TRUE} the simulation finishes gracefully when encountering a negative population size (i.e. does not throw an error). If \code{ignoreNegativeState=FALSE} the simulation stops with an error message when encountering a negative population size.}
-#' @param consoleInterval (approximate) interval at which \code{ssa} produces
-#'   simulation status output on the console (assumes \code{verbose=TRUE}).
-#'   If \code{consoleInterval=0} console output is generated each time step (or
-#'   tau-leap). If \code{consoleInterval=Inf} no console output is generated.
-#'   Note, \code{verbose=FALSE} disables all console output. \strong{Console
-#'   output drastically slows down simulations.}
+#' @param nd number of single-reaction steps performed using the Direct method
+#'  during `otl` suspension (See step 3, page 3, Cao et al. 2006).
+#' @param ignoreNegativeState boolean object indicating if negative state
+#'  values should be ignored (this can occur in the `etl` method).
+#'  If `ignoreNegativeState=TRUE` the simulation finishes gracefully when
+#'  encountering a negative population size (i.e. does not throw an error).
+#'  If `ignoreNegativeState=FALSE` the simulation stops with an error
+#'  message when encountering a negative population size.
+#' @param consoleInterval (approximate) interval at which `ssa` produces
+#'   simulation status output on the console (assumes `verbose=TRUE`).
+#'   If `consoleInterval=0` console output is generated each time step (or
+#'   tau-leap). If `consoleInterval=Inf` no console output is generated.
+#'   Note, `verbose=FALSE` disables all console output. **Console
+#'   output drastically slows down simulations.**
 #' @param censusInterval (approximate) interval between recording the state of the system.
-#'   If \code{censusInterval=0} \eqn{(t,x)} is recorded at each time step (or
-#'   tau-leap). If \code{censusInterval=Inf} only \eqn{(t_0,x_0)}{(t0,x0)}
+#'   If `censusInterval=0` \eqn{(t,x)} is recorded at each time step (or
+#'   tau-leap). If `censusInterval=Inf` only \eqn{(t_0,x_0)}{(t0,x0)}
 #'   and \eqn{(t_f,x_t)}{(tf,xf)} is recorded. Note, the size of the time
 #'   step (or tau-leaps) ultimately limits the interval between subsequent
 #'   recordings of the system state since the state of the system cannot be
 #'   recorded at a finer time interval the size of the time steps (or tau-leaps).
 #' @param verbose  boolean object indicating if the status of the simulation
-#'   simulation should be displayed on the console. If \code{verbose=TRUE}
+#'   simulation should be displayed on the console. If `verbose=TRUE`
 #'   the elapsed wall time and \eqn{(t,x)} is displayed on the console every
-#'   \code{consoleInterval} time step and a brief summary is displayed at
-#'   the end of the simulation. If \code{verbose=FALSE} the simulation runs
-#'   \emph{entirely} silent (overriding \code{consoleInterval}).
-#'   \strong{Verbose runs drastically slows down simulations.}
+#'   `consoleInterval` time step and a brief summary is displayed at
+#'   the end of the simulation. If `verbose=FALSE` the simulation runs
+#'   *entirely* silent (overriding `consoleInterval`).
+#'   **Verbose runs drastically slows down simulations.**
 #' @param maxWallTime maximum wall time duration (in seconds) that the
 #' simulation is allowed to run for before terminated. This option is
 #' useful, in particular, for systems that can end up growing uncontrolably.
@@ -103,23 +109,23 @@
 #'  * `stats$elapsedWallTime`: elapsed wall time in seconds.
 #'  * `stats$terminationStatus`: string vector listing the reason(s) for the
 #'    termination of the realization in 'plain words'. The possible termination statuses are:
-#'    - \code{finalTime} = if the simulation reached the maximum simulation time \code{tf},
-#'    - \code{extinction} = if the population size of all states is zero,
-#'    - \code{negativeState} = if one or several states have a negative population size (can occur in the ETL method),
-#'    - \code{zeroProp} = if all the states have a zero propensity function,
-#'    - \code{maxWallTime} = if the maximum wall time has been reached. Note the termination status may have more than one message.
+#'    - `finalTime` = if the simulation reached the maximum simulation time `tf`,
+#'    - `extinction` = if the population size of all states is zero,
+#'    - `negativeState` = if one or several states have a negative population size (can occur in the ETL method),
+#'    - `zeroProp` = if all the states have a zero propensity function,
+#'    - `maxWallTime` = if the maximum wall time has been reached. Note the termination status may have more than one message.
 #'  * `stats$nSteps`` total number of time steps (or tau-leaps) executed.
 #'  * `stats$meanStepSize`: mean step (or tau-leap) size.
 #'  * `stats$sdStepSize`: one standard deviation of the step (or tau-leap) size.
-#'  * `stats$SuspendedTauLeaps`: number of steps performed using the Direct method due to \code{OTL} suspension (only applicable for the \code{OTL} method).
-#'  * `arg$...`: sub-list with elements containing all the arguments and their values used to invoke \code{ssa} (see Usage and Arguments list above).
+#'  * `stats$SuspendedTauLeaps`: number of steps performed using the Direct method due to `OTL` suspension (only applicable for the `OTL` method).
+#'  * `arg$...`: sub-list with elements containing all the arguments and their values used to invoke `ssa` (see Usage and Arguments list above).
 #'
 #' @note Selecting the appropriate \acronym{SSA} method is a trade-off between
 #'   computational speed, accuracy of the results, and which \acronym{SSA}
 #'   actually works for a given scenario. This depends on the characteristics of
 #'   the defined system (e.g. number of reaction channels, number of species, and
 #'   the absolute and relative magnitude of the propensity functions).
-#'   \strong{Not all methods are appropriate for all models.} When selecting a
+#'   **Not all methods are appropriate for all models.** When selecting a
 #'   \acronym{SSA} method all of these factors have to be taken into
 #'   consideration. The various tau-leap methods accept a number of additional
 #'   arguments. While the default values of these arguments may work for some
@@ -128,13 +134,13 @@
 #'   substantial increase in efficiency may be gained by optimizing their values
 #'   for a specific system.
 #' @section Preparing a run: In order to invoke \acronym{SSA} the stochastic
-#'   model needs at least four components, the initial state vector (\code{x0}),
-#'   state-change matrix (\code{nu}), propensity vector (\code{a}), and the final
-#'   time of the simulation (\code{tf}). The initial state vector defines the
+#'   model needs at least four components, the initial state vector (`x0`),
+#'   state-change matrix (`nu`), propensity vector (`a`), and the final
+#'   time of the simulation (`tf`). The initial state vector defines the
 #'   population sizes in all the states at \eqn{t=0}, e.g. for a system with two
-#'   species \code{X1} and \code{X2} where both have an initial population size
-#'   of 1000 the initial state vector is defined as \code{x0 <-
-#'   c(X1=1000,X2=1000)}. The elements of the vector have to be labelled using
+#'   species `X1` and `X2` where both have an initial population size
+#'   of 1000 the initial state vector is defined as `x0 <-
+#'   c(X1=1000,X2=1000)`. The elements of the vector have to be labelled using
 #'   the same notation as the state variables used in the propensity functions.
 #'   The state-change matrix defines the change in the number of individuals in
 #'   each state (rows) as caused by one reaction of a given type (columns). For
@@ -143,15 +149,15 @@
 #'   \stackrel{c_1}{\longrightarrow} S_2}{S1 --c1--> S2} \deqn{S_2
 #'   \stackrel{c_2}{\longrightarrow} 0}{S2 --c2--> 0}
 #'
-#'   is defined as \code{nu <- matrix(c(-1,0,+1,-1),nrow=2,byrow=TRUE)} where
+#'   is defined as `nu <- matrix(c(-1,0,+1,-1),nrow=2,byrow=TRUE)` where
 #'   \eqn{c_1}{c1} and \eqn{c_2}{c2} are the per capita reaction probabilities.
-#'   The propensity vector, \code{a}, defines the probabilities that a particular
+#'   The propensity vector, `a`, defines the probabilities that a particular
 #'   reaction will occur over the next infinitesimal time interval \eqn{\left[
 #'   t,t+dt \right]}{[t,t+dt]}. For example, in the previous example the
-#'   propensity vector is defined as \code{a <- c("c1*X1","c2*X2")}. The
+#'   propensity vector is defined as `a <- c("c1*X1","c2*X2")`. The
 #'   propensity vector consists of character elements of each reaction's
 #'   propensity function where each state variable requires the corresponding
-#'   named element label in the initial state vector (\code{x0}).
+#'   named element label in the initial state vector (`x0`).
 #'
 #' @section Example: Irreversible isomerization:
 #' Perhaps the simplest model that can be formulated using the \acronym{SSA}
@@ -176,7 +182,7 @@
 #'   ssa.plot(out)
 #' }
 #'
-#' @seealso \link{GillespieSSA-package}, \code{\link{ssa.direct}}, \code{\link{ssa.etl}}, \code{\link{ssa.btl}}, \code{\link{ssa.otl}}
+#' @seealso [GillespieSSA-package], [ssa.direct()], [ssa.etl()], [ssa.btl()], [ssa.otl()]
 #'
 #' @keywords misc datagen ts
 #' @examples
